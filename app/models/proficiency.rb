6aa4b4b2a688615e7a6f2cda372cdf5f48939e92
Daniel Lu <dandydanny@gmail.com>
2014-02-06 13:26:54 -0600

    Add comments regarding join table

--- app/models/proficiency.rb
+++ app/models/proficiency.rb
@@ -1,4 +1,5 @@
 class Proficiency < ActiveRecord::Base
+  # join table
   belongs_to :user
   belongs_to :skills
 
--- db/migrate/20140206083704_create_proficiencies.rb
+++ db/migrate/20140206083704_create_proficiencies.rb
@@ -1,12 +1,12 @@
 class CreateProficiencies < ActiveRecord::Migration
   def change
     create_table :proficiencies do |t|
-      # t.belongs_to is important
+      # t.belongs_to is an important
+      # part of a join table
       t.belongs_to :user
       t.belongs_to :skill
       t.integer :experience
       t.boolean :education
-
       t.timestamps
     end
   end
